49c4b663923548def14948c86554cf89db96e0a6
Albert Tsao <awt46@sun-compute-01.cs.cornell.edu>
2023-07-16 15:53:12 -0400

    Fix paranthesis

--- run_example/run_rambo_relabel.py
+++ run_example/run_rambo_relabel.py
@@ -243,7 +243,7 @@ def train(args=get_args()):
         "tb": "tensorboard"
     }
     logger = Logger(log_dirs, output_config)
-    logger.log_hyperparameters((vars(args))
+    logger.log_hyperparameters(vars(args))
 
     # create policy trainer
     policy_trainer = MBPolicyTrainer(
